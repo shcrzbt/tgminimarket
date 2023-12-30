9a9f659e05346e90845041da7673f1495c78fbac
Шукур <sh.crazybeat@gmail.com>
2023-12-31 03:46:16 +0500

    some updates 2

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <script setup>
-	import { RouterView } from "vue-router";
+import { RouterView } from "vue-router"
+import WebApp from "@twa-dev/sdk"
+import { onMounted, ref } from "vue"
+
+const theme = ref('light')
+
+onMounted(()=> {
+	theme.value = WebApp.colorScheme
+})
 </script>
 
 <template>
+	<van-config-provider :theme="theme">
 	<RouterView />
+	</van-config-provider>
 </template>
