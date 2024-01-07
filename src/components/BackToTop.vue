baa1438ca6a569ba31cd359c7c75058dcd835c78
Шукур <sh.crazybeat@gmail.com>
2024-01-08 01:29:49 +0500

    back to top

--- src/components/BackToTop.vue
+++ src/components/BackToTop.vue
@@ -1,5 +1,5 @@
 <template>
-	<van-back-top style="--van-back-top-background: var(--main-secondary)" right="5vw" bottom="86">
+	<van-back-top style="--van-back-top-background: var(--main-secondary)" right="5vw" bottom="20">
 		<van-icon size="16" name="arrow-up" />
 	</van-back-top>
 </template>
