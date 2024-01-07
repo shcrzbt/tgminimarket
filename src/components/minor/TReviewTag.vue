3b6b59b57a1dd5e63868c43763478d00780c3fd5
Шукур <sh.crazybeat@gmail.com>
2024-01-07 19:04:52 +0500

    product-detail start 6

---
+++ src/components/minor/TReviewTag.vue
@@ -0,0 +1,39 @@
+<script setup>
+defineProps(["review"])
+</script>
+
+<template>
+	<div class="t-review-tag">
+		<div class="t-review-tag__stars">
+			<van-icon name="star" color="var(--orange)"></van-icon>
+			4.9
+		</div>
+
+		<div class="t-review-tag__count">(77 &nbspOценок)</div>
+	</div>
+</template>
+
+<style scoped lang="scss">
+.t-review-tag {
+	@include flex(.8rem, row, start, center);
+	@include getFont('p4');
+
+	font-weight: 200 !important;
+	box-shadow: var(--shadow-1);
+	border: 1px solid var(--bg-secondary-3);
+	background-color: var(--bg-primary);
+	padding: .4rem .8rem;
+	border-radius: .8rem;
+	white-space: nowrap;
+
+	&__stars {
+
+		color: var(--gs-100);
+	}
+
+	&__count {
+
+		color: var(--main-secondary-2);
+	}
+}
+</style>
---
+++ src/components/minor/TTag.vue
@@ -0,0 +1,27 @@
+<script setup>
+defineProps(["tag"])
+</script>
+
+<template>
+	<div class="t-tag">
+		<component v-if="tag.hasOwnProperty('render')" :is="tag.render" />
+		<span v-else>{{ tag.title }}</span>
+	</div>
+</template>
+
+<style scoped lang="scss">
+.t-tag {
+	@include getFont('p4');
+
+	font-weight: 200 !important;
+	border: 1px solid var(--main-secondary-lt-20);
+	padding: .2rem .8rem;
+	border-radius: .8rem;
+	white-space: nowrap;
+	//@include getFont('p4');
+	//font-size: 1rem !important;
+	//padding: 0 .4rem;
+	background-color: var(--main-secondary-lt-20);
+	color: var(--main-secondary);
+}
+</style>
